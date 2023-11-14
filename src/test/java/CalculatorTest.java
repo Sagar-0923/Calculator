074e6bb02796ce551728416f3226f359ec7f610d
Sagar-0923 <Ganga.Sagar@iiitb.org>
2023-11-14 14:47:56 +0530

    Webhooks

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -10,25 +10,25 @@ public class CalculatorTest {
     }
     @Test
     public void test_add() {
-        int a = 1;
-        int b = 4;
-        int expectedResult = 5;
+        int a = 10;
+        int b = 50;
+        int expectedResult = 60;
         int result = calculator.add(a,b);
         Assert.assertEquals(expectedResult, result);
     }
     @Test
     public void test_subt() {
-        int a = 50;
-        int b = 20;
-        int expectedResult = 30;
+        int a = 20;
+        int b = 5;
+        int expectedResult = 15;
         int result = calculator.subt(a,b);
         Assert.assertEquals(expectedResult, result);
     }
     @Test
     public void test_mul() {
-        int a = 2;
-        int b = 3;
-        int expectedResult = 6;
+        int a = 10;
+        int b = 2;
+        int expectedResult = 20;
         int result = calculator.mul(a,b);
         Assert.assertEquals(expectedResult, result);
     }
@@ -36,8 +36,8 @@ public class CalculatorTest {
     @Test
     public void test_per() {
         int a = 10;
-        int b = 3;
-        int expectedResult = 1;
+        int b = 4;
+        int expectedResult = 2;
         int result = calculator.per(a,b);
         Assert.assertEquals(expectedResult, result);
     }
